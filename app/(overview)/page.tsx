772a0fc539a56de0901d77dca3db0178a7c17adb
Radall <bj13_90@hotmail.com>
2023-10-15 17:28:10 +0200

    make first card wider

--- app/(overview)/page.tsx
+++ app/(overview)/page.tsx
@@ -27,8 +27,9 @@ export default async function Home() {
 
 function PostPreview(props: PropsWithChildren<{ post: PostDto; isFirst?: boolean }>) {
     const { post, isFirst } = props;
+    const cardClass = isFirst ? 'p-4 max-h-[400px] max-w-[600px] w-full' : 'p-4 max-h-[400px] max-w-[400px] w-full';
     return (
-        <Card className={`p-4 max-h-[400px] max-w-[400px] w-full`} shadow="md">
+        <Card className={cardClass} shadow="md">
             <NextLink href={`/${post.slug}`} className="block">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <h4 className="font-bold text-large">{post.title}</h4>
