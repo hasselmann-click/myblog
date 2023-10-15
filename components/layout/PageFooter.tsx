d261737374e40f3177a2d6f453b6d7ac99a5dccf
Radall <bj13_90@hotmail.com>
2023-10-15 17:32:39 +0200

    remove footer

--- components/layout/PageFooter.tsx
+++ components/layout/PageFooter.tsx
@@ -1,10 +1,5 @@
 import { Link } from '@nextui-org/link';
 
 export const PageFooter = () => {
-    return (
-        <Link className="gap-1 text-current" href="/tutorial" title="NextUI Tutorialpage">
-            <span className="text-default-500">Powered by</span>
-            <p className="text-primary">NextUI</p>
-        </Link>
-    );
+    
 };
