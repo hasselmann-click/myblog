eea69b44df929dfbeeea04e5c17d223f06304f35
radall <radall@outlook.com>
2023-12-09 20:57:14 +0000

    add github icon

--- components/layout/PageFooter.tsx
+++ components/layout/PageFooter.tsx
@@ -1,14 +1,19 @@
-import { RssFilledIcon } from '@/components/icons';
+import { GithubIcon, RssIcon } from '@/components/icons';
 import { Link } from '@nextui-org/link';
 
-const PATH = '/rss';
+const PATH_RSS = '/rss';
+const PATH_GH = 'https://github.com/hasselmann-click/myblog';
 
 export const PageFooter = () => {
     return (
         <>
-            <Link href={PATH}>
-                <RssFilledIcon />
+            <Link href={PATH_RSS}>
+                <RssIcon />
                 <small style={{ paddingTop: '6px' }}>RSS</small>
+            </Link>
+            <Link href={PATH_GH}>
+                <GithubIcon />
+                <small style={{ paddingTop: '6px' }}>Github</small>
             </Link>
         </>
     );
